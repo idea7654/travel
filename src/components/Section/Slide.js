d0df4e26ef77cce0c3dae68c7f1d901627d71b53
idea7654 <idea7654@naver.com>
2021-01-03 22:47:06 +0900

    first commit

---
+++ src/components/Section/Slide.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+
+const Slide = () => {
+    useEffect(() => {
+        
+        
+    }, []);
+    return (
+        <div className="h-screen">
+            
+        </div>
+    );
+}
+
+export default Slide;
